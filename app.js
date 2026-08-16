96f9fcdd7f1f37215125b95f62d306a29b7426fd
d4rjhhkzvm-rgb <d4rjhhkzvm@privaterelay.appleid.com>
2026-08-17 02:25:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const shoes = JSON.parse(localStorage.getItem("shoes") || "[]");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 
-function add() {
+const firebaseConfig = {
+  apiKey: "AIzaSyBPiLbUPClwrvbvzmvEaaIu2FaAWWsA4TI",
+  authDomain: "centeralbaziki.firebaseapp.com",
+  projectId: "centeralbaziki",
+  storageBucket: "centeralbaziki.firebasestorage.app",
+  messagingSenderId: "368079353950",
+  appId: "1:368079353950:web:adb51c325e4208aba59ccf",
+  measurementId: "G-C5FG364ZDR"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+async function add() {
   const shoe = {
     model: document.getElementById("model").value,
     supplier: document.getElementById("supplier").value,
@@ -8,15 +23,20 @@ function add() {
     qty: document.getElementById("qty").value
   };
 
-  shoes.push(shoe);
-  localStorage.setItem("shoes", JSON.stringify(shoes));
+  await addDoc(collection(db, "shoes"), shoe);
+
   show();
 }
 
-function show() {
+
+async function show() {
   let html = "";
 
-  shoes.forEach((shoe) => {
+  const querySnapshot = await getDocs(collection(db, "shoes"));
+
+  querySnapshot.forEach((doc) => {
+    const shoe = doc.data();
+
     html += `
       <div class="item">
         <b>الموديل:</b> ${shoe.model}<br>
@@ -29,5 +49,6 @@ function show() {
 
   document.getElementById("list").innerHTML = html;
 }
+
 
 show();
