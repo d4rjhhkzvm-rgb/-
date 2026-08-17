1399baa58896f2876f15c592f671c87a9f2c50ee
d4rjhhkzvm-rgb <d4rjhhkzvm@privaterelay.appleid.com>
2026-08-17 11:50:06 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,20 @@
-firebase-config.js
+// Firebase Configuration - CENTER ALBAZIKI
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPiLbUPClwrvbvzmvEaaIu2FaAWWsA4TI",
+  authDomain: "centeralbaziki.firebaseapp.com",
+  projectId: "centeralbaziki",
+  storageBucket: "centeralbaziki.firebasestorage.app",
+  messagingSenderId: "368079353950",
+  appId: "1:368079353950:web:adb51c325e4208aba59ccf",
+  measurementId: "G-C5FG364ZDR"
+};
+
+
+// تشغيل Firebase
+firebase.initializeApp(firebaseConfig);
+
+// الخدمات
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
